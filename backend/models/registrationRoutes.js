eddacfd048a84be184815209991da1159c000e6d
AlineHub-tech <umugwanezaaline77@gmail.com>
2025-11-13 09:10:44 -0800

    Delete backend/models/registrationRoutes.js

--- backend/models/registrationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerCourse, myCourses } from "../controllers/registrationController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-router.post("/", protect, registerCourse);
-router.get("/mine", protect, myCourses);
-
-export default router;

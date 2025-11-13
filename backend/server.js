1762ad96ac24614f48cfe1a0078ac880d63358e5
AlineHub-tech <umugwanezaaline77@gmail.com>
2025-11-13 09:16:21 -0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import registrationRoutes from "./routes/registrationRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/register", registrationRoutes);
+
+app.listen(process.env.PORT, () =>
+  console.log('Server running on port ${process.env.PORT}')
+);
+
